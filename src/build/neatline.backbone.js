accb86a230716f41b0e9529eaf695a3543f49321
David McClure <david.mcclure@virginia.edu>
2012-12-21 14:02:58 -0500

    Build directory.

---
+++ src/build/neatline.backbone.js
@@ -0,0 +1,19 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * Backbone.Neatline ~ Backbone.js extensions written for Neatline.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+Backbone.Neatline = (function(Backbone, _) {
+  var Neatline = {};
+
+//= ../neatline.immortalview.js
+
+  return Neatline;
+})(Backbone, _, $ || window.jQuery || window.Zepto || window.ender);
